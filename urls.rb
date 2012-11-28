429d000ea597194d9cf415c44939f9eab7ee3657
Jamie Wilkinson <jamie@jamiedubs.com>
2012-11-27 23:47:44 -0600

    Put urls in their own directories

--- urls.rb
+++ urls.rb
@@ -8,17 +8,20 @@ pp targets
 targets.each do |group,urls|
 
   puts "Processing #{urls.length} sites for #{group} ..."
-  FileUtils.mkdir_p("stats/#{group}")
 
   urls.each do |url|
     puts "#{url} ..."
+
+    dir = "stats/#{group}/#{url}"
+    FileUtils.mkdir_p(dir)
+
     output = `bundle exec ruby url_stats.rb #{url}`
     # datestamp = Time.now.strftime('%Y_%m_%d')
     datestamp = Time.now.strftime('%Y_%m_%d_%H%M')
     puts output
 
     # Write to a datestamped JSON file, we'll do more with this later
-    filename = "stats/#{group}/#{datestamp}.json"
+    filename = "#{dir}/#{datestamp}.json"
     File.open(filename, 'w') { |file| file.write(output) }
   end
 end
